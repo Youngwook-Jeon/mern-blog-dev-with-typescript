2e74cc395ffafb9308fad178f70d689f2920618d
Young wook Jeon <ela87@naver.com>
2021-08-27 15:34:06 +0900

    Send email to activate an account

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -3,6 +3,10 @@ import Users from '../models/userModel';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { generateActiveToken } from '../config/generateToken';
+import sendEmail from '../config/sendMail';
+import { validateEmail } from '../middleware/valid';
+
+const CLIENT_URL = `${process.env.BASE_URL}`;
 
 const authController = {
     register: async(req: Request, res: Response) => {
@@ -15,13 +19,12 @@ const authController = {
             const newUser = { name, account, password: passwordHash };
 
             const active_token = generateActiveToken({ newUser });
+            const url = `${CLIENT_URL}/active/${active_token}`;
 
-            res.json({ 
-                status: 'OK',
-                msg: 'Registration is successful', 
-                data: newUser,
-                active_token, 
-            });
+            if (validateEmail(account)) {
+                sendEmail(account, url, 'Verify your email address');
+                return res.json({ msg: 'Success! Please check your email.' });
+            }
         } catch (err) {
             return res.status(500).json({ msg: err.message });
         }
