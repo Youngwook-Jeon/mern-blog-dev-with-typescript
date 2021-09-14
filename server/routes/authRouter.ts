0756e6f6d487776abc86e04f6af2def0aa7a16f5
Young wook Jeon <ela87@naver.com>
2021-09-15 00:29:22 +0900

    Login user / Refresh token

--- server/routes/authRouter.ts
+++ server/routes/authRouter.ts
@@ -6,5 +6,8 @@ const router = express.Router();
 
 router.post('/register', validRegister, authController.register);
 router.post('/activate', authController.activateAccount);
+router.post('/login', authController.login);
+router.get('/logout', authController.logout);
+router.get('/refresh_token', authController.refreshToken);
 
 export default router;
