4e2e716794400a9ae36f81140ee3f9f8cac98e04
Young wook Jeon <ela87@naver.com>
2021-09-15 13:15:55 +0900

    Add a PageRenderer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import PageRender from './PageRender';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello Client</h1>
+    <div className="container">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={PageRender} />
+          <Route exact path="/:page" component={PageRender} />
+          <Route exact path="/:page/:slug" component={PageRender} />
+        </Switch>
+      </Router>
     </div>
   );
 };
