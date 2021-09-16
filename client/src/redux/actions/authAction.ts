746e6b2145310d622fd4b465df1388181356061d
Young wook Jeon <ela87@naver.com>
2021-09-17 02:18:36 +0900

    Login

---
+++ client/src/redux/actions/authAction.ts
@@ -0,0 +1,19 @@
+import { Dispatch } from 'redux';
+import { AUTH, IAuthType } from '../types/authTypes';
+import { IUserLogin } from '../../utils/TypeScript';
+import { postAPI } from '../../utils/FetchData';
+
+export const login = (userLogin: IUserLogin) => async (dispatch: Dispatch<IAuthType>) => {
+  try {
+    const res = await postAPI('login', userLogin);
+    dispatch({
+      type: AUTH,
+      payload: {
+        token: res.data.access_token,
+        user: res.data.user
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
