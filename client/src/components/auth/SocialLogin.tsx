277c6e1e8952474bc65f915a98c053f9c3299ae4
Young wook Jeon <ela87@naver.com>
2021-09-19 22:05:13 +0900

    Login with Facebook

--- client/src/components/auth/SocialLogin.tsx
+++ client/src/components/auth/SocialLogin.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { GoogleLogin, GoogleLoginResponse } from 'react-google-login-lite';
-import { googleLogin } from '../../redux/actions/authAction';
+import { googleLogin, facebookLogin } from '../../redux/actions/authAction';
+import { FacebookLogin, FacebookLoginAuthResponse } from 'react-facebook-login-lite';
 
 const SocialLogin = () => {
   const dispatch = useDispatch();
@@ -11,15 +12,34 @@ const SocialLogin = () => {
     dispatch(googleLogin(id_token));
   }
 
+  const onFBSuccess = (response: FacebookLoginAuthResponse) => {
+    const { accessToken, userID } = response.authResponse;
+    dispatch(facebookLogin(accessToken, userID));
+  }
+  
+  const onFailure = (error: any) => {
+    console.log(error);
+  }
+
   return (
-    <div className="my-2">
-      <GoogleLogin 
-        client_id='773941006200-i55fn7k0op57rdu19pmomej5nv9nnvnu.apps.googleusercontent.com'
-        cookiepolicy='single_host_origin'
-        onSuccess={onSuccess}
-        longtitle={false}
-      />
-    </div>
+    <>
+      <div className="my-2">
+        <GoogleLogin 
+          client_id='773941006200-i55fn7k0op57rdu19pmomej5nv9nnvnu.apps.googleusercontent.com'
+          cookiepolicy='single_host_origin'
+          onSuccess={onSuccess}
+          longtitle={false}
+        />
+      </div>
+
+      <div className="my-2">
+        <FacebookLogin 
+          appId="930059841054718"
+          onSuccess={onFBSuccess}
+          onFailure={onFailure}
+        />
+      </div>
+    </>
   );
 };
 
