da3e5fac5e9f40f86a21514c8e816ff06ecfbc30
Young wook Jeon <ela87@naver.com>
2021-09-29 20:25:36 +0900

    Get blogs written by a particular user

--- client/src/redux/actions/blogAction.ts
+++ client/src/redux/actions/blogAction.ts
@@ -2,7 +2,7 @@ import { Dispatch } from 'redux';
 import { IBlog } from '../../utils/TypeScript';
 import { imageUpload } from '../../utils/ImageUpload';
 import { ALERT, IAlertType } from '../types/alertTypes';
-import { GET_HOME_BLOGS, IGetHomeBlogsType, GET_BLOGS_CATEGORY_ID, IGetBlogsCategoryType } from '../types/blogTypes';
+import { GET_HOME_BLOGS, IGetHomeBlogsType, GET_BLOGS_CATEGORY_ID, IGetBlogsCategoryType, IGetBlogsUserType, GET_BLOGS_USER_ID } from '../types/blogTypes';
 import { postAPI, getAPI } from '../../utils/FetchData';
 
 export const createBlog = (blog: IBlog, token: string) => async (dispatch: Dispatch<IAlertType>) => {
@@ -53,6 +53,25 @@ export const getBlogsByCategoryId = (id: string, search: string) => async (dispa
     
     dispatch({
       type: GET_BLOGS_CATEGORY_ID,
+      payload: { ...res.data, id, search }
+    });
+
+    dispatch({ type: ALERT, payload: { loading: false }});
+  } catch (err: any) {
+    dispatch({ type: ALERT, payload: { errors: err.response.data.msg }});
+  }
+}
+
+export const getBlogsByUserId = (id: string, search: string) => async (dispatch: Dispatch<IAlertType | IGetBlogsUserType>) => {
+  try {
+    let limit = 3;
+    let value = search ? search : `?page=${1}`;
+    dispatch({ type: ALERT, payload: { loading: true }});
+
+    const res = await getAPI(`blogs/user/${id}${value}&limit=${limit}`);
+
+    dispatch({
+      type: GET_BLOGS_USER_ID,
       payload: { ...res.data, id, search }
     });
 
--- client/src/redux/reducers/index.ts
+++ client/src/redux/reducers/index.ts
@@ -5,6 +5,7 @@ import category from './categoryReducer';
 import homeBlogs from './homeBlogsReducer';
 import blogsCategory from './blogsCategoryReducer';
 import otherInfo from './otherInfoReducer';
+import blogsUser from './blogsUserReducer';
 
 export default combineReducers({
   auth,
@@ -12,5 +13,6 @@ export default combineReducers({
   category,
   homeBlogs,
   blogsCategory,
-  otherInfo
+  otherInfo,
+  blogsUser
 })
