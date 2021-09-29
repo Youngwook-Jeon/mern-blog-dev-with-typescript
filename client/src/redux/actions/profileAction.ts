0d179eb3a0548913e1ee0eeeab7e2fac9c37ee31
Young wook Jeon <ela87@naver.com>
2021-09-29 18:22:46 +0900

    Info page of other users

--- client/src/redux/actions/profileAction.ts
+++ client/src/redux/actions/profileAction.ts
@@ -2,8 +2,9 @@ import { Dispatch } from 'redux';
 import { AUTH, IAuth, IAuthType } from '../types/authTypes';
 import { IAlertType, ALERT } from '../types/alertTypes';
 import { checkImage, imageUpload } from '../../utils/ImageUpload';
-import { patchAPI } from '../../utils/FetchData';
+import { patchAPI, getAPI } from '../../utils/FetchData';
 import { checkPassword } from '../../utils/Validators';
+import { GET_OTHER_INFO, IGetOtherInfoType } from '../types/profileTypes';
 
 export const updateUser = (avatar: File, name: string, auth: IAuth) => async (dispatch: Dispatch<IAlertType | IAuthType>) => {
   if (!auth.access_token || !auth.user) return;
@@ -52,6 +53,23 @@ export const resetPassword = (password: string, cf_password: string, token: stri
 
     const res = await patchAPI('reset_password', { password }, token);
     dispatch({ type: ALERT, payload: { success: res.data.msg }});  
+  } catch (err: any) {
+    dispatch({ type: ALERT, payload: { errors: err.response.data.msg }});
+  }
+}
+
+export const getOtherInfo = (id: string) => async (dispatch: Dispatch<IAlertType | IGetOtherInfoType>) => {
+  try {
+    dispatch({ type: ALERT, payload: { loading: true }});  
+
+    const res = await getAPI(`user/${id}`);
+    
+    dispatch({
+      type: GET_OTHER_INFO,
+      payload: res.data
+    })
+
+    dispatch({ type: ALERT, payload: {  }});  
   } catch (err: any) {
     dispatch({ type: ALERT, payload: { errors: err.response.data.msg }});
   }
--- client/src/redux/reducers/index.ts
+++ client/src/redux/reducers/index.ts
@@ -4,11 +4,13 @@ import alert from './alertReducer';
 import category from './categoryReducer';
 import homeBlogs from './homeBlogsReducer';
 import blogsCategory from './blogsCategoryReducer';
+import otherInfo from './otherInfoReducer';
 
 export default combineReducers({
   auth,
   alert,
   category,
   homeBlogs,
-  blogsCategory
+  blogsCategory,
+  otherInfo
 })
