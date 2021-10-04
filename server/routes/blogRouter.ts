7669a3f79ed9ee7c5a6e4f623493ed1793e89744
Young wook Jeon <ela87@naver.com>
2021-10-04 19:11:46 +0900

    Get blog details

--- server/routes/blogRouter.ts
+++ server/routes/blogRouter.ts
@@ -8,6 +8,7 @@ router.post('/blog', auth, blogController.createBlog);
 router.get('/home/blogs', blogController.getHomeBlogs);
 router.get('/blogs/category/:id', blogController.getBlogsByCategory);
 router.get('/blogs/user/:id', blogController.getBlogsByUser);
+router.get('/blog/:id', blogController.getBlog);
 
 
 export default router;
