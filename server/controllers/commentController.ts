80090c7d9b71f1f9f1fed72dfe5a316a3c690f8c
Young wook Jeon <ela87@naver.com>
2021-10-04 20:26:14 +0900

    Add comment model

---
+++ server/controllers/commentController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import Comments from '../models/commentModel';
+import { IReqAuth } from '../config/interface';
+
+const commentController = {
+  createComment: async (req: IReqAuth, res: Response) => {
+    if (!req.user) return res.status(400).json({ msg: "Invalid Authorization" });
+
+    try {
+      const { content, blog_id, blog_user_id } = req.body;
+
+      const newComment = new Comments({ content, blog_id, blog_user_id });
+      await newComment.save();
+      
+      return res.json(newComment);
+    } catch (err: any) {
+      return res.status(500).json({ msg: err.message });
+    }
+  }
+};
+
+export default commentController;
