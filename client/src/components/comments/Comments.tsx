ed25e522b6747c24a6995154b444e3af55f4255c
Young wook Jeon <ela87@naver.com>
2021-10-05 14:08:00 +0900

    Frontend: Show reply comments

--- client/src/components/comments/Comments.tsx
+++ client/src/components/comments/Comments.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IComment } from '../../utils/TypeScript';
 import AvatarComment from './AvatarComment';
+import AvatarReply from './AvatarReply';
 import CommentList from './CommentList';
 
 interface IProps {
@@ -8,12 +9,25 @@ interface IProps {
 }
 
 const Comments: React.FC<IProps> = ({ comment }) => {
+  const [showReply, setShowReply] = useState<IComment[]>([]);
+
   return (
     <div className="my-3 d-flex" 
       style={{ opacity: comment._id ? 1 : 0.5, pointerEvents: comment._id ? 'initial' : 'none' }}
     >
       <AvatarComment user={comment.user} />
-      <CommentList comment={comment} />
+      <CommentList comment={comment} showReply={showReply} setShowReply={setShowReply} >
+        {
+          showReply.map((comment, index) => (
+            <div key={index} className="my-3 d-flex" 
+              style={{ opacity: comment._id ? 1 : 0.5, pointerEvents: comment._id ? 'initial' : 'none' }}
+            >
+              <AvatarReply user={comment.user} reply_user={comment.reply_user} />
+              <CommentList comment={comment} showReply={showReply} setShowReply={setShowReply} />
+            </div>
+          ))
+        }
+      </CommentList>
     </div>
   );
 };
